38eaeb86ee9bb196b86e3b0acd7d266c60345aee
Yahyaikrxm <Yahyaikrxm@gmail.com>
2025-06-24 01:24:32 -0400

    cleaning up template

---
+++ my-portfolio/src/Home/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LiaPoopSolid } from "react-icons/lia";
+
+function App() {
+  return (
+    <div className="App">
+      <LiaPoopSolid />
+    </div>
+  );
+}
+
+export default App;
