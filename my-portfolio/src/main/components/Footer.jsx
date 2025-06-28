3508637767883847bd16069ef234db404e86b690
Yahyaikrxm <Yahyaikrxm@gmail.com>
2025-06-27 21:52:40 -0400

    Footer adjustments

--- my-portfolio/src/main/components/Footer.jsx
+++ my-portfolio/src/main/components/Footer.jsx
@@ -5,12 +5,21 @@ function Footer() {
     return (
         <div>
             <div className="Top">
-                <h1 className="contact-me-text">Want to get in touch?</h1>
-                <div className="footer-top-sub">
-                <a href="https//apple.com" className="link-footer">GitHub</a>
-                <a href="https//apple.com" className="link-footer">LinkDen</a>
-                <p className="link-footer">Yahyaikrxm@gmail.com</p>
-                <p className="link-footer">+1 437-268-1551</p>
+                <div className="footer-top-1">
+                    <h1 className="contact-me-text">Want to get in touch?</h1>
+                </div>
+                <div className="footer-top-2">
+                    <div className="footer-top-links">
+                        <div className="links-footer-1">
+                        <p className="link-footer">Yahyaikrxm@gmail.com</p>
+                        <p className="link-footer">+1 437-268-1551</p>
+                        </div>
+                        <div className="vertical-line"></div>
+                        <div className="links-footer-2">
+                        <a href="https//apple.com" className="link-footer">GitHub</a>
+                        <a href="https//apple.com" className="link-footer">LinkDen</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="Bottom">
