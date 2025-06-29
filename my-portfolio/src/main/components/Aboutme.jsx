9d871883865f679cb9cf17c86982922c5e42fa71
Yahyaikrxm <Yahyaikrxm@gmail.com>
2025-06-28 22:03:44 -0400

    sub components del, icons added, and about card added

--- my-portfolio/src/main/components/Aboutme.jsx
+++ my-portfolio/src/main/components/Aboutme.jsx
@@ -6,10 +6,13 @@ function Aboutme() {
     return (
         <div className="about-me-main">      
             <div className="about-card-and-photo">
-            <h1 className="about-title">ABOUT ME</h1> 
+            <h1 className="about-title">ABOUT ME</h1>
+            <div className="about-me-card">
             <p className="about-text">Im an passionate high-school student and aspiring engineer, I enjoy web development, electronics, photography, and gaming. When im nto soing that i like F1 and wathcing soccer, I find the most joy in math and just vibing I love story games and stuff like that, you know what i mena so yeah thats me and thats more about me in highshcoo currently and working towards universty</p>
             <img src={pic} alt="Me" className="pic-of-me"/>
             </div>
+            </div>
+
         </div>
     );
 }
--- my-portfolio/src/main/components/Projects.jsx
+++ my-portfolio/src/main/components/Projects.jsx
@@ -1,16 +1,68 @@
 import React from "react";
 import "./Projects.css"; 
-import Projectone from "./subcomponents/Projectone"
-import Projecttwo from "./subcomponents/Projecttwo"
-import Projectthree from "./subcomponents/Projectthree"
+import imgpro1 from '../../assets/pic-of-me.JPG';
+import imgpro2 from '../../assets/pic-of-me.JPG';
+import imgpro3 from '../../assets/pic-of-me.JPG';
 
 function Projects() {
     return (
         <div>
             <h1 className="projects-heading">PROJECTS</h1>
-            <Projectone />
-            <Projecttwo />
-            <Projectthree />
+            <div className="project-cards">
+            <div className="project-card-1">
+           
+           {/* Project card 1 */}
+            <div className="project-pic-div">
+             <img src={imgpro1} alt="Me" className="project-pic1"/>  
+            </div>
+            <div className="project-card-right">  
+                <div className="project-card-right-1">
+                    <h2 className="project-card-head">ARDUINO FLIP CLOCK</h2>
+                    <h3 className="project-card-subhead">AutoCAD | React.JS | SwiftOS | 3D Printing</h3>
+                    <p className="project-card-text">I’ve been doing photography as a hobby on and off and i find lots of fun in capturing the moment and I do my best to edit them - i find the most enjoyment in that. I love doing photography and wish to continue to more in the future . . .</p>
+                </div> 
+                <div className="project-card-right-2">
+                    <button className="read-more-btn">READ MORE</button>
+                </div>
+            </div>
+        </div>
+
+        {/* Project card 2 */}
+        <div className="project-card-2">
+            <div className="project-pic-div">
+             <img src={imgpro2} alt="Me" className="project-pic2"/>  
+            </div>
+            <div className="project-card-right">  
+                <div className="project-card-right-1">
+                    <h2 className="project-card-head">ARDUINO FLIP CLOCK</h2>
+                    <h3 className="project-card-subhead">AutoCAD | React.JS | SwiftOS | 3D Printing</h3>
+                    <p className="project-card-text">I’ve been doing photography as a hobby on and off and i find lots of fun in capturing the moment and I do my best to edit them - i find the most enjoyment in that. I love doing photography and wish to continue to more in the future . . .</p>
+                </div> 
+                <div className="project-card-right-2">
+                    <button className="read-more-btn-2">READ MORE</button>
+                </div>
+            </div>
+        </div>
+
+        {/* Project card 3 */}
+        <div className="project-card-3">
+            <div className="project-pic-div">
+             <img src={imgpro3} alt="Me" className="project-pic3"/>  
+            </div>
+            <div className="project-card-right">  
+                <div className="project-card-right-1">
+                    <h2 className="project-card-head">ARDUINO FLIP CLOCK</h2>
+                    <h3 className="project-card-subhead">AutoCAD | React.JS | SwiftOS | 3D Printing</h3>
+                    <p className="project-card-text">I’ve been doing photography as a hobby on and off and i find lots of fun in capturing the moment and I do my best to edit them - i find the most enjoyment in that. I love doing photography and wish to continue to more in the future . . .</p>
+                </div> 
+                <div className="project-card-right-2">
+                    <button className="read-more-btn-3">READ MORE</button>
+                </div>
+            </div>
+        </div>
+        
+        
+            </div>
         </div>
     );
 }
